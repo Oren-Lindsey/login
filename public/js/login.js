2f59410081dc112111cabd616e00e3aae2ae51d6
Oren Lindsey <67929847+Oren-Lindsey@users.noreply.github.com>
2022-02-24 11:29:53 -0500

    Added folder and login.js

---
+++ public/js/login.js
@@ -0,0 +1,40 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const password = document.getElementById('password');
+const error = document.getElementById('error')
+const message = document.getElementById('message')
+
+username.addEventListener('invalid', invalid);
+password.addEventListener('invalid', invalid);
+form.addEventListener('submit', formSubmit);
+
+function invalid(event) {
+  error.removeAttribute('hidden');
+}
+
+function formSubmit(event) {
+  var reqData = {'username': username.value, 'password': password.value};
+  var stringifiedData = JSON.stringify(reqData)
+  const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: stringifiedData
+    };
+  fetch('https://login-test.s40.repl.co/session', requestOptions)
+    .then(res => res.json())
+    .then(data => loginRes(data))
+  event.preventDefault();
+}
+function loginRes(data) {
+  if (data.hasOwnProperty('error')) {
+    error.innerText = `Error: ${data.error}`
+    if (error.hasAttribute('hidden')) {
+      error.removeAttribute('hidden');
+    }
+  } else {
+    message.innerText = 'Logged in successfully!'
+    message.removeAttribute('hidden');
+    document.cookie = `authorization=${data.token}`
+    window.location.replace('/logged-in')
+  }
+}
