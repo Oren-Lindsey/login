17a2d187360a2f2c682f6d8c65640998798ea72c
Oren Lindsey <67929847+Oren-Lindsey@users.noreply.github.com>
2022-03-18 15:14:03 -0400

    Update newaccount.js

--- public/js/newaccount.js
+++ public/js/newaccount.js
@@ -19,12 +19,25 @@ function formSubmit(event) {
         headers: { 'Content-Type': 'application/json' },
         body: stringifiedData
     };
-  fetch('https://login-test.s40.repl.co/create-account', requestOptions)
+  fetch('/create-account', requestOptions)
     .then(res => res.json())
     .then(data => handleRes(data))
   event.preventDefault();
 }
 function handleRes(data) {
-  document.cookie = `authorization=${data.token}`
-  window.location.replace('/logged-in')
+  if (data.hasOwnProperty('message')) {
+    document.cookie = `authorization=${data.token}`
+    window.location.replace('/')
+  } else if (data.hasOwnProperty('error')) {
+    if (data.error == 'must match regex /^[a-z0-9_\\-]{1,20}$/') {
+      error.innerText = 'Error! usernames must be between 1 and 20 characters long and can only contain letters, numbers, underscores, and dashes.'
+      error.removeAttribute('hidden')
+    } else {
+      error.innerText = data.error
+      error.removeAttribute('hidden')
+    }
+  } else {
+    error.innerText = 'An unknown error occurred. Please try again'
+    error.removeAttribute('hidden')
+  }
 }
